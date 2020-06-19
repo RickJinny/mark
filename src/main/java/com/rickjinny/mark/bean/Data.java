cf68a9fe67870603b2a233eadf497fd5c770c260
RickJinny <rickjinny@126.com>
2020-06-20 00:00:36 +0800

    update
    update

--- src/main/java/com/rickjinny/mark/bean/Data.java
+++ src/main/java/com/rickjinny/mark/bean/Data.java
@@ -7,12 +7,16 @@ public class Data {
     @Getter
     private static int counter = 0;
 
+    private static Object locker = new Object();
+
     public static int reset() {
         counter = 0;
         return counter;
     }
 
-    public synchronized void wrong() {
-        counter++;
+    public void wrong() {
+        synchronized (locker) {
+            counter++;
+        }
     }
 }
