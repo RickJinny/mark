8dffece1c53f98d910ade2eb3b6d4c7750b0a954
RickJinny <rickjinny@126.com>
2020-06-19 23:52:43 +0800

    update
    update

--- src/main/java/com/rickjinny/mark/controller/T02_LockController.java
+++ src/main/java/com/rickjinny/mark/controller/T02_LockController.java
@@ -1,8 +1,12 @@
 package com.rickjinny.mark.controller;
 
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.IntStream;
 
 @RestController
 @RequestMapping("/lock")
@@ -12,7 +16,7 @@ public class T02_LockController {
     private volatile int a = 1;
     private volatile int b = 1;
 
-    public void add() {
+    public synchronized void add() {
         log.info("add start");
         for (int i = 0; i < 10000; i++) {
             a++;
@@ -21,7 +25,7 @@ public class T02_LockController {
         log.info("add done");
     }
 
-    public void compare() {
+    public synchronized void compare() {
         log.info("compare start");
         for (int i = 0; i < 10000; i++) {
             // a 始终等于 b 吗？
@@ -38,4 +42,34 @@ public class T02_LockController {
         new Thread(() -> lock.add()).start();
         new Thread(() -> lock.compare()).start();
     }
+
+
+    /**
+     * 2、
+     */
+    @Getter
+    private static int counter = 0;
+
+    public static int reset() {
+        counter = 0;
+        return counter;
+    }
+
+    public synchronized void wrong() {
+        counter++;
+    }
+
+    @RequestMapping("/wrong")
+    public int wrong(@RequestParam(value = "count", defaultValue = "1000000") int counter) {
+        reset();
+        // 多线程循环，一定次数调用不同实例的 wrong 方法
+        IntStream.rangeClosed(1, counter).parallel()
+                .forEach(i -> wrong());
+        return getCounter();
+    }
+
+
+
+
+
 }
