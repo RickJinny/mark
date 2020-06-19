09d8f847217e84b3f4d8cc683e64db2fd4a54268
RickJinny <rickjinny@126.com>
2020-06-20 00:15:59 +0800

    update
    update

--- src/main/java/com/rickjinny/mark/controller/T02_LockController.java
+++ src/main/java/com/rickjinny/mark/controller/T02_LockController.java
@@ -7,7 +7,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
+
+import static java.lang.System.*;
 
 @RestController
 @RequestMapping("/lock")
@@ -44,7 +49,6 @@ public class T02_LockController {
         new Thread(() -> lock.compare()).start();
     }
 
-
     /**
      * 2、
      */
@@ -55,5 +59,49 @@ public class T02_LockController {
         IntStream.rangeClosed(1, counter).parallel()
                 .forEach(i -> new Data().wrong());
         return Data.getCounter();
+    }
+
+    /**
+     * 3、精细化加锁，只对 List<Integer> 对象加锁
+     */
+    private List<Integer> data = new ArrayList<>();
+
+    // 不涉及共享资源的慢方法
+    private void slow() {
+        try {
+            TimeUnit.MILLISECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 错误的加锁方法
+    @RequestMapping("/wrong3")
+    public int wrong3() {
+        long begin = System.currentTimeMillis();
+        IntStream.rangeClosed(1, 1000).parallel().forEach(i -> {
+            // 加锁粒度太粗了
+            synchronized (this) {
+                slow();
+                data.add(i);
+            }
+        });
+        log.info("took:{}", System.currentTimeMillis() - begin);
+        return data.size();
+    }
+
+    // 正确的加锁方法
+    @RequestMapping("/right3")
+    public int right3() {
+        long begin = System.currentTimeMillis();
+        IntStream.rangeClosed(1,1000).parallel().forEach(i -> {
+            slow();
+            // 只对 List 加锁
+            synchronized (data) {
+                data.add(i);
+            }
+        });
+        log.info("took:{}", System.currentTimeMillis() - begin);
+        return data.size();
     }
 }
