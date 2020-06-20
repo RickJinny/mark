36f1c79b24eb94528edeb7b1164b504a9c5a444c
RickJinny <rickjinny@126.com>
2020-06-20 09:22:30 +0800

    update
    update

--- src/main/java/com/rickjinny/mark/controller/T03_ThreadPoolController.java
+++ src/main/java/com/rickjinny/mark/controller/T03_ThreadPoolController.java
@@ -1,19 +1,52 @@
 package com.rickjinny.mark.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @RestController
 @RequestMapping("/pool")
+@Slf4j
 public class T03_ThreadPoolController {
 
     @RequestMapping("/oom1")
-    public void oom1() {
+    public void oom1() throws InterruptedException {
         ThreadPoolExecutor threadPool = (ThreadPoolExecutor) Executors.newFixedThreadPool(1);
         // 打印线程池的信息, 稍后我会解释这段代码
+        printStats(threadPool);
+        for (int i = 0; i < 100_000_000; i++) {
+            threadPool.execute(() -> {
+                String payload = IntStream.rangeClosed(1, 1_000_000)
+                        .mapToObj(__ -> "a")
+                        .collect(Collectors.joining("")) + UUID.randomUUID().toString();
+                try {
+                    TimeUnit.HOURS.sleep(1);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                log.info(payload);
+            });
+        }
 
+        threadPool.shutdown();
+        threadPool.awaitTermination(1, TimeUnit.HOURS);
+    }
+
+    private void printStats(ThreadPoolExecutor threadPool) {
+        Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(() -> {
+            log.info("====================");
+            log.info("Pool Size: {}", threadPool.getPoolSize());
+            log.info("Active Threads: {}", threadPool.getActiveCount());
+            log.info("Number of Tasks Completed: {}", threadPool.getCompletedTaskCount());
+            log.info("Number of Tasks in Queue: {}", threadPool.getQueue().size());
+            log.info("====================");
+        }, 0, 1, TimeUnit.SECONDS);
     }
 }
