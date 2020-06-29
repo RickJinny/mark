affbf47563c3edac201dd4e7b2fe88ea205a3407
RickJinny <rickjinny@126.com>
2020-06-29 16:23:09 +0800

    update

---
+++ src/main/java/com/rickjinny/mark/controller/p31_java8/T31_02_StreamTest.java
@@ -0,0 +1,82 @@
+package com.rickjinny.mark.controller.p31_java8;
+
+import com.google.common.collect.Lists;
+import org.junit.Test;
+
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalDouble;
+
+/**
+ * 使用 Java8 简化代码:
+ * 1、使用 Stream 简化集合操作。
+ * 2、使用 Optional 简化判空逻辑。
+ * 3、JDK8 结合 Lambda 和 Stream 对各种类的增强。
+ */
+public class T31_02_StreamTest {
+
+    private static double calc(List<Integer> ints) {
+        // 临时中间集合
+        List<Point2D> point2DList = new ArrayList<>();
+        for (Integer i : ints) {
+            point2DList.add(new Point2D.Double((double) i % 3, (double) i / 3));
+        }
+        // 临时变量，纯粹是为了获得最后的结果需要的中间变量
+        double total = 0;
+        int count = 0;
+        for (Point2D point2D : point2DList) {
+            // 过滤
+            if (point2D.getY() > 1) {
+                // 算距离
+                double distance = point2D.distance(0, 0);
+                total += distance;
+                count++;
+            }
+        }
+        // 注意 count 可能为 0 的可能
+        return count > 0 ? total / count : 0;
+    }
+
+    /**
+     * 使用 Stream 流, 来简化 calc(List<Integer> ints) 方法的代码逻辑
+     */
+    @Test
+    public void stream() {
+        ArrayList<Integer> ints = Lists.newArrayList(1, 2, 3, 4, 5, 6, 7, 8);
+        double result = ints.stream()
+                .map(i -> new Point2D.Double((double) i % 3, (double) i / 3))
+                .filter(point -> point.getY() > 1)
+                .mapToDouble(point -> point.distance(0, 0))
+                .average()
+                .orElse(0);
+        System.out.println(result);
+
+    }
+
+    /**
+     * 2、使用 Optional 简化判空逻辑。
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void optional() {
+        // 通过 get 方法获取 Optional 中的设计值
+        Integer integer = Optional.of(1).get();
+
+        // 通过 ofNullable 来初始化一个 null, 通过 orElse 方法实现 Optional 中无数据的时候返回一个默认值
+        Optional.ofNullable(null).orElse("A");
+
+        // OptionalDouble 是基本类型 double 的 Optional 对象，isParent判断有数据
+        OptionalDouble.empty().isPresent();
+
+        // 通过 map 方法，可以对 Optional 对象进行级联转换, 不会出现空指针, 转换后还是一个 Optional
+        Integer integer1 = Optional.of(1).map(Math::incrementExact).get();
+
+        // 通过 filter 方法实现 Optional 中数据的过滤, 得到一个 Optional, 然后级联使用 orElse 提供默认值
+        Integer integer2 = Optional.of(1).filter(i -> i % 2 == 0).orElse(null);
+
+        // 通过 orElseThrow 实现无数据时抛出异常
+        Optional.empty().orElseThrow(IllegalAccessError::new);
+
+    }
+}
