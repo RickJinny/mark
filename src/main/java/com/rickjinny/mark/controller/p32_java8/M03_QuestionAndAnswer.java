d2bef0254c5efe0eedcc9aaa3c19f7184a048d1f
RickJinny <rickjinny@126.com>
2020-06-30 22:36:02 +0800

    update
    update

---
+++ src/main/java/com/rickjinny/mark/controller/p32_java8/M03_QuestionAndAnswer.java
@@ -0,0 +1,15 @@
+package com.rickjinny.mark.controller.p32_java8;
+
+/**
+ * 1、使用 Stream 可以非常方便地对 List 做各种操作, 那么有没有什么办法可以实现在整个过程中观察数据变化呢？
+ *    比如：我们进行 filter + map 操作，如果观察 filter 后 map 的原始数据呢？
+ *
+ * 2、Collector 类提供了很多现成的收集器, 那我们有没有办法实现自定义的收集器呢？
+ *    比如：实现一个 MostPopularCollector, 来得到 List 中出现次数最多的元素, 满足下面的两个测试用例
+ *
+ */
+public class M03_QuestionAndAnswer {
+
+    
+
+}
