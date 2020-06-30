53f44f25c6374d50c7adcc39f5fdfadff19b3b9b
RickJinny <rickjinny@126.com>
2020-06-30 10:08:07 +0800

    update stream
    update stream

--- src/main/java/com/rickjinny/mark/controller/p32_java8/GenerateStreamTest.java
+++ src/main/java/com/rickjinny/mark/controller/p32_java8/GenerateStreamTest.java
@@ -4,6 +4,7 @@ import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
+import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.stream.Stream;
 
@@ -39,5 +40,22 @@ public class GenerateStreamTest {
         Stream.of(1, 2, "a").map(item -> item.getClass().getName()).forEach(System.out::println);
     }
 
+    /**
+     * 第三种方法: 通过 Stream.iterate 方法使用迭代的方式构造一个无限流, 然后使用 limit 限制流元素个数。
+     */
+    @Test
+    public void iterate() {
+        Stream.iterate(2, item -> item * 2).limit(10).forEach(System.out::println);
+        Stream.iterate(BigInteger.ZERO, n -> n.add(BigInteger.TEN)).limit(10).forEach(System.out::println);
+    }
+
+    /**
+     * 第四种方法: 通过 Stream.generate 方法从外部传入一个提供元素的 Supplier 来构造无限流, 然后使用 limit 限制流元素个数。
+     */
+    @Test
+    public void generate() {
+        Stream.generate(() -> "test").limit(3).forEach(System.out::println);
+        Stream.generate(Math::random).limit(10).forEach(System.out::println);
+    }
 
 }
