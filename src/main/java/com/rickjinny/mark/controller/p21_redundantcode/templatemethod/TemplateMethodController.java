8b8601e03e789bd554167a9838eccc0587b41a43
RickJinny <rickjinny@126.com>
2020-07-04 17:15:13 +0800

    update
    update

--- src/main/java/com/rickjinny/mark/controller/p21_redundantcode/templatemethod/TemplateMethodController.java
+++ src/main/java/com/rickjinny/mark/controller/p21_redundantcode/templatemethod/TemplateMethodController.java
@@ -7,6 +7,7 @@ import com.rickjinny.mark.controller.p21_redundantcode.templatemethod.right.Abst
 import com.rickjinny.mark.controller.p21_redundantcode.templatemethod.wrong.InternalUserCart;
 import com.rickjinny.mark.controller.p21_redundantcode.templatemethod.wrong.VipUserCart;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,12 +24,13 @@ public class TemplateMethodController {
 
     private static Map<Long, Integer> items = new HashMap<>();
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     static {
         items.put(1L, 2);
         items.put(2L, 4);
     }
-
-    private ApplicationContext applicationContext;
 
     @GetMapping("/wrong")
     public Cart wrong(@RequestParam("userId") Long userId) {
@@ -55,8 +57,17 @@ public class TemplateMethodController {
 
     @GetMapping("/right")
     public Cart right(@RequestParam("userId") Long userId) {
+        /**
+         * 在定义三个购物车子类时, 我们在 @Service 注解中对 Bean 进行了命名。既然三个购物车都叫 XXXUserCart，那我们就可以
+         * 把用户类型字符串拼接 UserCart 构成购物车 Bean 的名称，然后利用 Spring 的 IOC 容器，通过 Bean 的名称直接获取到
+         * AbstractCart, 调用 process 方法即可实现通用。
+         * 其实这就是工厂模式, 只不过借助 Spring 容器实现罢了。
+         */
+        // 把用户类型字符串拼接 UserCart 构成购物车 Bean 的名称
         String userCategory = DB.getUserCategory(userId);
-        AbstractCart cart = (AbstractCart) applicationContext.getBean(userCategory + "userCategory");
+        // 利用 Spring 的 IOC 容器，通过 Bean 的名称直接获取到 AbstractCart 对象
+        AbstractCart cart = (AbstractCart) applicationContext.getBean(userCategory + "UserCart");
+        // AbstractCart 对象，调用 process 方法即可实现通用
         return cart.process(userId, items);
     }
 }
