fb26f2299e0092251f8b1043305cc67a62eacf75
RickJinny <rickjinny@126.com>
2020-07-05 10:15:56 +0800

    asyc
    asyc

---
+++ src/main/java/com/rickjinny/mark/controller/p22_apidesign/t04_apiasyncsyncmode/FileService.java
@@ -0,0 +1,10 @@
+package com.rickjinny.mark.controller.p22_apidesign.t04_apiasyncsyncmode;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FileService {
+
+    
+
+}
---
+++ src/main/java/com/rickjinny/mark/controller/p22_apidesign/t04_apiasyncsyncmode/Main.java
@@ -0,0 +1,4 @@
+package com.rickjinny.mark.controller.p22_apidesign.t04_apiasyncsyncmode;
+
+public class Main {
+}
