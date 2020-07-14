8f58c0f1ecc03a7ef3916315633971882bf2b396
RickJinny <rickjinny@126.com>
2020-07-14 08:00:15 +0800

    update
    update

--- src/main/java/com/rickjinny/mark/controller/p11_null/avoidnullpointerexception/AvoidNullPointerExceptionController.java
+++ src/main/java/com/rickjinny/mark/controller/p11_null/avoidnullpointerexception/AvoidNullPointerExceptionController.java
@@ -11,7 +11,7 @@ import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 @RestController
-@RequestMapping("avoidnullpointerexception")
+@RequestMapping("avoidNull")
 @Slf4j
 public class AvoidNullPointerExceptionController {
 
