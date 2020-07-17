7e7b4be5c63de60b9b788a6ed3ff5bb696c87df6
RickJinny <rickjinny@126.com>
2020-07-17 08:16:53 +0800

    update
    update

--- src/main/java/com/rickjinny/mark/controller/p06_transaction/UserRepository.java
+++ src/main/java/com/rickjinny/mark/controller/p06_transaction/UserRepository.java
@@ -1,11 +1,12 @@
 package com.rickjinny.mark.controller.p06_transaction;
 
 import com.rickjinny.mark.controller.p06_transaction.bean.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity, Long>{
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
     List<UserEntity> findByName(String name);
 }
