e1f243a535898c238db4d69a0f74b54218a9dbce
RickJinny <rickjinny@126.com>
2020-07-18 09:25:54 +0800

    update
    update

--- src/main/java/com/rickjinny/mark/controller/p06_transaction/transactionProxyFailed/TransactionProxyFailedController.java
+++ src/main/java/com/rickjinny/mark/controller/p06_transaction/transactionProxyFailed/TransactionProxyFailedController.java
@@ -1,5 +1,7 @@
 package com.rickjinny.mark.controller.p06_transaction.transactionProxyFailed;
 
+import com.rickjinny.mark.controller.p06_transaction.bean.UserEntity;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/transactionProxyFailed")
+@Slf4j
 public class TransactionProxyFailedController {
 
     @Autowired
@@ -17,4 +20,26 @@ public class TransactionProxyFailedController {
         return userService.createUserWrong1(name);
     }
 
+    @RequestMapping(value = "/wrong2")
+    public int wrong2(@RequestParam("name") String name) {
+        return userService.createWrong2(name);
+    }
+
+    /**
+     * 使用这个方法调用，比较奇怪
+     */
+    @RequestMapping(value = "/right1")
+    public int right1(@RequestParam("name") String name) {
+        return userService.createUserRight(name);
+    }
+
+    @RequestMapping(value = "/right2")
+    public int right2(@RequestParam("name") String name) {
+        try {
+            userService.createUserPublic(new UserEntity(name));
+        } catch (Exception e) {
+            log.error("create user failed because {}", e.getMessage());
+        }
+        return userService.getUserCount(name);
+    }
 }
--- src/main/java/com/rickjinny/mark/controller/p06_transaction/transactionProxyFailed/UserService.java
+++ src/main/java/com/rickjinny/mark/controller/p06_transaction/transactionProxyFailed/UserService.java
@@ -73,7 +73,30 @@ public class UserService {
         }
     }
 
-    private int getUserCount(String name) {
+    @Autowired
+    private UserService self;
+
+    /**
+     * 我们尝试修改下 UserService 的代码，注入一个 self，然后再通过 self 实例调用标记有 @Transactional 注解的 createUserPublic 方法。
+     * 设置断点可以看到，self 是由 Spring 通过 CgLib 方式增强过的类：
+     * (1) CgLib 通过继承方式实现代理类, private 方法在子类不可见，自然也就无法进行事务增强。
+     * (2) this 指针代表对象自己, Spring 不可能注入 this，所以通过 this 访问方法必然不是代理。
+     *
+     * 把 this 换成 self 后, 测试发现：在 Controller 中调用 createUserRight 方法可以验证事务是生效的，非法的用户注册操作可以回滚。
+     *
+     * 虽然在 UserService 内部注入自己调用自己的 createUserPublic 可以正确实现事务，但更合理的实现方式是，让 Controller 直接调用之前定义的
+     * UserService 的 createUserPublic 方法，因为注入自己调用自己很奇怪，也不符合分层实现的规范。
+     */
+    public int createUserRight(String name) {
+        try {
+            self.createUserPublic(new UserEntity(name));
+        } catch (Exception e) {
+            log.error("create user failed beacause {}", e.getMessage());
+        }
+        return userRepository.findByName(name).size();
+    }
+
+    public int getUserCount(String name) {
         return userRepository.findByName(name).size();
     }
 }
