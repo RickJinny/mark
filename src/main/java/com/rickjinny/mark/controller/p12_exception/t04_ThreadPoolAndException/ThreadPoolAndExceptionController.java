3ccfee8cca48bae58758f80c37d52900a9e6716e
RickJinny <rickjinny@126.com>
2020-07-19 19:03:49 +0800

    update
    update

--- src/main/java/com/rickjinny/mark/controller/p12_exception/t04_ThreadPoolAndException/ThreadPoolAndExceptionController.java
+++ src/main/java/com/rickjinny/mark/controller/p12_exception/t04_ThreadPoolAndException/ThreadPoolAndExceptionController.java
@@ -27,8 +27,8 @@ public class ThreadPoolAndExceptionController {
      * 因为没有手动捕获异常进行处理，ThreadGroup 帮我们进行了未捕获异常的默认处理，向标准错误输出打印了出现异常的线程名称和异常信息。
      * 显然，这种没有以统一的错误日志格式记录错误信息打印出来的形式，对生产代码是不合适的。
      */
-    @GetMapping(value = "/execute")
-    public void execute() throws InterruptedException {
+    @GetMapping(value = "/executeWrong")
+    public void executeWrong() throws InterruptedException {
         String prefix = "test";
         ExecutorService threadPool = Executors.newFixedThreadPool(1,
                 new ThreadFactoryBuilder().setNameFormat(prefix + "%d").build());
@@ -40,4 +40,35 @@ public class ThreadPoolAndExceptionController {
         threadPool.shutdown();
         threadPool.awaitTermination(1, TimeUnit.HOURS);
     }
+
+    /**
+     *
+     * 第一种修复的方法：
+     * (1)、以 execute 方法提交到线程池的异步任务，最好在任务内部做好异常处理。
+     * (2)、设置自定义的异常处理程序作为保底，比如在声明线程池时自定义线程池的未捕获异常处理程序。
+     */
+    @GetMapping(value = "/executeRight")
+    public void executeRight() throws InterruptedException {
+        String prefix = "test";
+        ExecutorService threadPool = Executors.newFixedThreadPool(1, new ThreadFactoryBuilder()
+                .setNameFormat(prefix + "%d")
+                .setUncaughtExceptionHandler((thread, throwable) -> log.error("ThreadPool {} got exception", thread, throwable))
+                .build());
+        // 提交 10 个任务到线程池处理，第 5 个任务会抛出运行时异常
+        IntStream.rangeClosed(1, 10).forEach(i -> threadPool.execute(() -> {
+            if (i == 5) throw new RuntimeException("error");
+            log.info("I am done: {}", i);
+        }));
+        threadPool.shutdown();
+        threadPool.awaitTermination(1, TimeUnit.HOURS);
+    }
+
+    /**
+     * 第二种修复的方法：设置全局的默认未捕获异常处理程序。
+     */
+    static {
+        Thread.setDefaultUncaughtExceptionHandler((thread, throwable) -> log.error("Thread {} got exception", thread, throwable));
+    }
+
+
 }
