59bbd016945f23f9fcde123d56f504270b4f02bd
RickJinny <rickjinny@126.com>
2020-07-22 23:53:47 +0800

    update
    update

--- src/main/java/com/rickjinny/mark/controller/p14_io/t03_FileBufferPerformance/FileBufferPerformance.java
+++ src/main/java/com/rickjinny/mark/controller/p14_io/t03_FileBufferPerformance/FileBufferPerformance.java
@@ -34,6 +34,10 @@ public class FileBufferPerformance {
      * 为了简化逻辑便于理解，这里我们不对数据进行处理，直接把原文件数据写入目标文件，相当于文件复制。
      *
      * 这样的实现，复制一个 35MB 的文件居然耗时 190 秒。
+     *
+     * 分析：每读取一个字节，每写入一个字节都进行一次 IO 操作，代价太大了。
+     *
+     * 解决方案：考虑使用缓冲区作为过渡，一次性从原文件读取一定数量的数据到缓冲区，一次性写入一定数量的数据到目标文件。
      */
     private static void perByteOperation() throws IOException {
         Files.deleteIfExists(Paths.get("dest.txt"));
@@ -42,6 +46,24 @@ public class FileBufferPerformance {
             int i;
             while ((i = fileInputStream.read()) != -1) {
                 fileOutputStream.write(i);
+            }
+        }
+    }
+
+    /**
+     * 解决方案：考虑使用缓冲区作为过渡，一次性从原文件读取一定数量的数据到缓冲区，一次性写入一定数量的数据到目标文件。
+     *
+     * 修改后，使用 100 字节作为缓冲区，使用 FileInputStream 的 byte[] 的重载来一次性读取一定字节的数据，同时使用 FileOutputStream 的
+     * byte[] 的重载实现一次性从缓冲区写入一定字节的数据到文件。
+     */
+    private static void bufferOperationWith100Buffer() throws IOException {
+        Files.deleteIfExists(Paths.get("dest.txt"));
+        try (FileInputStream fileInputStream = new FileInputStream("src.txt");
+             FileOutputStream fileOutputStream = new FileOutputStream("dest.txt")) {
+            byte[] buffer = new byte[100];
+            int len = 0;
+            while ((len = fileInputStream.read(buffer)) != -1) {
+                fileOutputStream.write(buffer, 0, len);
             }
         }
     }
