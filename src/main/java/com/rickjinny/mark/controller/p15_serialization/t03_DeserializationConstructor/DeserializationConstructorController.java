fdcfa3794f9043e080e267c743f179f2ae562476
RickJinny <rickjinny@126.com>
2020-07-25 00:05:11 +0800

    11
    11

--- src/main/java/com/rickjinny/mark/controller/p15_serialization/t03_DeserializationConstructor/DeserializationConstructorController.java
+++ src/main/java/com/rickjinny/mark/controller/p15_serialization/t03_DeserializationConstructor/DeserializationConstructorController.java
@@ -15,6 +15,11 @@ public class DeserializationConstructorController {
     @Autowired
     private ObjectMapper objectMapper;
 
+    /**
+     * 得到的结果都是 false。
+     * 出现这个问题的原因：默认情况下，在反序列化的时候，Jackson框架只会调用无参构造方法创建对象。
+     * 如果走自定义的构造方法创建对象，需要通过 @JsonCreator 来指定构造方法，并通过 @JsonProperty 设置构造方法中参数对应的 JSON 属性名。
+     */
     @RequestMapping(value = "/wrong")
     public void wrong() throws JsonProcessingException {
         log.info("result : {}", objectMapper.readValue("{\"code\":1234}", APIResultWrong.class));
