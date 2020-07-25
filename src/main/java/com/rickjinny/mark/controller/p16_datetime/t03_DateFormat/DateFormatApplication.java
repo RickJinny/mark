a29f5f9ea432489609d186bc70a6df5acaf8ac2b
RickJinny <rickjinny@126.com>
2020-07-25 15:53:51 +0800

    date
    date

--- src/main/java/com/rickjinny/mark/controller/p16_datetime/t03_DateFormat/DateFormatApplication.java
+++ src/main/java/com/rickjinny/mark/controller/p16_datetime/t03_DateFormat/DateFormatApplication.java
@@ -8,6 +8,9 @@ import java.time.format.DateTimeFormatterBuilder;
 import java.time.temporal.ChronoField;
 import java.util.Calendar;
 import java.util.Locale;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 3、日期时间格式化和解析
@@ -39,7 +42,6 @@ public class DateFormatApplication {
 
     /**
      * 初始化一个 Calendar，设置日期时间为 2019年12月29日，使用大写的 YYYY 来初始化 SimpleDateFormat
-     * <p>
      * default Locale: fr_FR
      * 格式化: 2020-12-29
      * weekYear: 2020
@@ -80,18 +82,99 @@ public class DateFormatApplication {
         System.out.println("result: " + dateFormat.parse(dateString));
     }
 
-    private static void better() {
-        LocalDateTime localDateTime = LocalDateTime.parse("2020/1/2 12:09:28.789", dateTimeFormatter);
-        System.out.println(localDateTime.format(dateTimeFormatter));
+    /**
+     * SimpleDateFormat 第一个坑
+     *
+     * 定义 static SimpleDateFormat 可能会带来线程安全问题。比如像这样，使用一个 100 线程的线程池，循环 20 次把时间格式化任务
+     * 提交到线程池处理，每个任务中又循环 10 次解析 2020-01-01 11:22:33 这样一个时间表示。
+     *
+     * 运行程序后大量报错，没有报错的输出结果也不正常。
+     *
+     * SimpleDateFormat 的作用是：定义解析和格式化日期时间的模式。这看起来是一次性的工作，应该复用，但它的解析和格式化操作是非线程安全的。
+     *
+     * 经过源码的分析，format 方法，只能在同一个线程复用 SimpleDateFormat，比较好的解决方案是通过 ThreadLocal 来存放 SimpleDateFormat。
+     */
+    private static void wrong2() throws InterruptedException {
+        ExecutorService threadPool = Executors.newFixedThreadPool(100);
+        for (int i = 0; i < 20; i++) {
+            // 提交 20 个并发解析时间的任务到线程池，模拟并发环境
+            threadPool.execute(() -> {
+                for (int j = 0; j < 10; j++) {
+                    try {
+                        System.out.println(simpleDateFormat.parse("2020-01-01 11:22:33"));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
 
-        String dateTime = "20160901";
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyyMM");
-        System.out.println("result : " + dateTimeFormatter.parse(dateTime));
+        threadPool.shutdown();
+        threadPool.awaitTermination(1, TimeUnit.HOURS);
     }
 
-    public static void main(String[] args) throws ParseException {
+    /**
+     * 使用 ThreadLocal 来存放 SimpleDateFormat。
+     */
+    private static void right2() throws InterruptedException {
+        ExecutorService threadPool = Executors.newFixedThreadPool(100);
+        for (int i = 0; i < 20; i++) {
+            // 提交 20 个并发解析时间的任务到线程池，模拟并发环境
+            threadPool.execute(() -> {
+                for (int j = 0; j < 10; j++) {
+                    try {
+                        System.out.println(threadSafeSimpleDateFormat.get().parse("2020-01-01 11:22:33"));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+
+        threadPool.shutdown();
+        threadPool.awaitTermination(1, TimeUnit.HOURS);
+    }
+
+
+    /**
+     * SimpleDateFormat 第二坑是：当需要解析的字符串和格式不匹配时，SimpleDateFormat 表现得很宽容。
+     *
+     * 结果居然输出了 2091年1月1日，原始是把0901当成了月份，相当于是75年。
+     *
+     * result : Mon Jan 01 00:00:00 CST 2091
+     */
+    private static void wrong3() throws ParseException {
+//        LocalDateTime localDateTime = LocalDateTime.parse("2020/1/2 12:09:28.789", dateTimeFormatter);
+//        System.out.println(localDateTime.format(dateTimeFormatter));
+
+        String dateTime = "20160901";
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMM");
+        System.out.println("result : " + dateFormat.parse(dateTime));
+    }
+
+    /**
+     * SimpleDateFormat 第三坑，我们使用 Java8 中的 DateTimeFormatter 就可以避免过去。
+     * 首先，使用 DateTimeFormatterBuilder 来定义格式化字符串，不用去记忆使用大写的 Y 还是小写的 y，大写的 M 还是小写的 m。
+     *
+     * 其次，DateTimeFormatter 是线程安全的，可以定义为 static 使用，最后 DateTimeFormatter 的解析比较严格，需要解析的字符串和格式不匹配时，
+     * 会直接报错，而不会把 0901 解析为月份。
+     */
+    private static void wrong4() throws ParseException {
+        // 使用刚才定义的 DateTimeFormatterBuilder 构建的 DateTimeFormatter 来解析这个时间
+        LocalDateTime localDateTime = LocalDateTime.parse("2020/1/2 12:09:28.789", dateTimeFormatter);
+        // 解析成功
+        System.out.println(localDateTime.format(dateTimeFormatter));
+        // 使用 yyyy 格式解析 20160901 是否可以成功呢？
+        String dateTime = "20160901";
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMM");
+        System.out.println("result : " + dateFormat.parse(dateTime));
+    }
+
+    public static void main(String[] args) throws ParseException, InterruptedException {
 //        wrong1();
-        right1();
-        better();
+//        right1();
+//        wrong2();
+//        right2();
+        wrong3();
     }
 }
