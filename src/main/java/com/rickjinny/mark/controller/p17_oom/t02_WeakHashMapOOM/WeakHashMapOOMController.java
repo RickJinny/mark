5e251a25ce92e0d3edabc7e591774eb22e6713a8
RickJinny <rickjinny@126.com>
2020-07-26 10:26:12 +0800

    oom
    oom

--- src/main/java/com/rickjinny/mark/controller/p17_oom/t02_WeakHashMapOOM/WeakHashMapOOMController.java
+++ src/main/java/com/rickjinny/mark/controller/p17_oom/t02_WeakHashMapOOM/WeakHashMapOOMController.java
@@ -1,9 +1,11 @@
 package com.rickjinny.mark.controller.p17_oom.t02_WeakHashMapOOM;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.ConcurrentReferenceHashMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.ref.WeakReference;
 import java.util.Map;
 import java.util.WeakHashMap;
 import java.util.concurrent.Executors;
@@ -49,6 +51,68 @@ public class WeakHashMapOOMController {
         LongStream.rangeClosed(1, 2000000).forEach(i -> {
             User user = new User(userName + i);
             cache.put(user, new UserProfile(user, "location" + i));
+        });
+    }
+
+
+    /**
+     * 在 wrong() 方法中，使用 Map<User, UserProfile> cache = new WeakHashMap<>();
+     * WeakHashMap 的 key 虽然是弱引用，但是其 value 却持有 key 中对象的强引用，value 被
+     * Entry 引用，Entry 被 WeakHashMap 引用，最终导致 key 无法回收。
+     *
+     * 第一种解决方案：就是让 value 变为弱引用，使用 WeakReference 来包装 UserProfile 即可。
+     */
+    private Map<User, WeakReference<UserProfile>> cache2 = new WeakHashMap<>();
+
+    /**
+     * 重新运行程序，从日志观察到 cache size 不再是固定的 200万，而是不断减少，甚至在手动 GC 后所有的 Entry 都被回收了。
+     */
+    @RequestMapping(value = "/right1")
+    public void right1() {
+        String userName = "haha";
+        // 间隔1秒定时输出缓存中的条目数
+        Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(
+                () -> log.info("cache size : {}", cache2.size()), 1, 1, TimeUnit.SECONDS);
+
+        LongStream.rangeClosed(1, 2000000).forEach(i -> {
+            User user = new User(userName + i);
+            // 这次，我们使用弱引用来包装 UserProfile
+            cache2.put(user, new WeakReference<>(new UserProfile(user, "location" + i)));
+        });
+    }
+
+    /**
+     * 第二种解决方案：让 value 也就是 UserProfile 不再引用 key，而是重新 new 出一个新的 User 对象赋值给 UserProfile。
+     */
+    @RequestMapping(value = "/right2")
+    public void right2() {
+        String userName = "haha";
+
+        Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(
+                () -> log.info("cahche size : {}", cache.size()), 1, 1, TimeUnit.SECONDS);
+
+        LongStream.rangeClosed(1, 2000000).forEach(i -> {
+            User user = new User(userName + i);
+            cache.put(user, new UserProfile(new User(user.getName()), "location" + i));
+        });
+    }
+
+    /**
+     * 第三种解决方案：Spring 提供的 ConcurrentReferenceHashMap 类可以使用弱引用、软引用做缓存，key 和 value 同时被
+     * 软引用或弱引用包装，也能解决相互引用导致的数据不能释放问题。与 WeakHashMap 相比，ConcurrentReferenceHashMap 不但
+     * 性能更好，还可以确保线程安全。
+     */
+    private Map<User, UserProfile> cache03 = new ConcurrentReferenceHashMap<>();
+
+    @RequestMapping(value = "right3")
+    public void right3() {
+        String userName = "right3";
+        Executors.newSingleThreadScheduledExecutor().scheduleAtFixedRate(
+                () -> log.info("cache size: {}", cache03.size()), 1, 1, TimeUnit.SECONDS);
+
+        LongStream.rangeClosed(1, 20000000).forEach(i -> {
+            User user = new User(userName + i);
+            cache03.put(user, new UserProfile(user, "location" + i));
         });
     }
 }
