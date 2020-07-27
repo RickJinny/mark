54065694eb8068a5cb9008599e8972e4bdcd3f8f
RickJinny <rickjinny@126.com>
2020-07-28 00:18:00 +0800

    update
    update

--- src/main/java/com/rickjinny/mark/controller/p19_spring/t01_BeanSingletonAndOrder/BeanSingletonAndOrderController.java
+++ src/main/java/com/rickjinny/mark/controller/p19_spring/t01_BeanSingletonAndOrder/BeanSingletonAndOrderController.java
@@ -1,14 +1,33 @@
 package com.rickjinny.mark.controller.p19_spring.t01_BeanSingletonAndOrder;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequestMapping(value = "/beanSingletonAndOrder")
 @RestController
 @Slf4j
 public class BeanSingletonAndOrderController {
 
+    @Autowired
+    private List<SayService> sayServiceList;
 
+    @Autowired
+    private ApplicationContext applicationContext;
 
+    @RequestMapping(value = "test")
+    public void test() {
+        log.info("============================");
+        sayServiceList.forEach(SayService::say);
+    }
+
+    @RequestMapping(value = "/test2")
+    public void test2() {
+        log.info("============================");
+        applicationContext.getBeansOfType(SayService.class).values().forEach(SayService::say);
+    }
 }
--- src/main/java/com/rickjinny/mark/controller/p19_spring/t01_BeanSingletonAndOrder/SayHello.java
+++ src/main/java/com/rickjinny/mark/controller/p19_spring/t01_BeanSingletonAndOrder/SayHello.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Service;
 
 /**
  * 在 SayHello 和 SayBye 类上加了 @Service 注解，让他们成为 Bean, 也没有考虑到父类是有状态的。
+ *
+ * 正确的处理方式：在为类标记上 @Service 注解，把类型交由容器管理前，首先评估一下类是否有状态，然后为 Bean 设置合适的 Scope。
+ * 将该类设置生命周期 value = ConfigurableBeanFactory.SCOPE_PROTOTYPE。
  */
 @Service
 @Slf4j
