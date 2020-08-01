c45c0fb935e58520431b43b4c4cb29d74bddbc58
RickJinny <rickjinny@126.com>
2020-08-01 18:47:55 +0800

    update
    update

--- src/main/java/com/rickjinny/mark/controller/p27_clientdata/t01_TrustClientCalculation/TrustClientCalculationController.java
+++ src/main/java/com/rickjinny/mark/controller/p27_clientdata/t01_TrustClientCalculation/TrustClientCalculationController.java
@@ -1,5 +1,6 @@
 package com.rickjinny.mark.controller.p27_clientdata.t01_TrustClientCalculation;
 
+import com.rickjinny.mark.controller.p27_clientdata.t01_TrustClientCalculation.bean.CreateOrderRequest;
 import com.rickjinny.mark.controller.p27_clientdata.t01_TrustClientCalculation.bean.Item;
 import com.rickjinny.mark.controller.p27_clientdata.t01_TrustClientCalculation.bean.Order;
 import lombok.extern.slf4j.Slf4j;
@@ -17,9 +18,13 @@ public class TrustClientCalculationController {
 
     @PostMapping(value = "/wrongOrder")
     public void wrongOrder(@RequestBody Order order) {
-        this.createOrder();
+        this.createOrder(order);
     }
 
+    /**
+     * 创建订单：前端传递过来的商品价格和总价的信息，这些信息只能用来呈现和校对。
+     * 服务端也一定要重新从数据库来初始化商品的价格，重新计算最终的订单价格。如果不怎么做的话，很可能会被黑客利用，商品总价被恶意修改为比较低的价格。
+     */
     @PostMapping(value = "/rightOrder")
     public void rightOrder(@RequestBody Order order) {
         // 根据商品id，重新查询商品
@@ -39,10 +44,24 @@ public class TrustClientCalculationController {
         // 重新设置商品总价
         order.setItemPrice(totalPrice);
         // 创建订单
-        createOrder();
+        createOrder(order);
     }
 
-    private void createOrder() {
+    /**
+     * 第二种方法：前端传入需要的信息。定义一个 CreateOrderRequest 的 POJO 类做为接口入参。
+     */
+    @PostMapping(value = "/rightOrder2")
+    public Order rightOrder2(@RequestBody CreateOrderRequest createOrderRequest) {
+        // 商品id和商品数量是可信的没有问题，其他数据需要由服务端计算
+        Item item = DBUtils.getItem(createOrderRequest.getItemId());
+        Order order = new Order();
+        order.setItemPrice(item.getItemPrice());
+        order.setItemTotalPrice(item.getItemPrice().multiply(BigDecimal.valueOf(createOrderRequest.getQuantity())));
+        createOrder(order);
+        return order;
+    }
+
+    private void createOrder(Order order) {
 
     }
 }
--- src/main/java/com/rickjinny/mark/controller/p27_clientdata/t01_TrustClientCalculation/bean/CreateOrderRequest.java
+++ src/main/java/com/rickjinny/mark/controller/p27_clientdata/t01_TrustClientCalculation/bean/CreateOrderRequest.java
@@ -4,5 +4,8 @@ import lombok.Data;
 
 @Data
 public class CreateOrderRequest {
-
+    // 商品id
+    private Long itemId;
+    // 商品数量
+    private int quantity;
 }
