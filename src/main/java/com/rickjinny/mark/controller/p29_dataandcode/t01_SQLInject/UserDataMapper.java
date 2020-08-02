b56b3ac5c80555d2783e168f92905c4a7b69b72e
RickJinny <rickjinny@126.com>
2020-08-03 00:45:16 +0800

    dao
    dao

--- src/main/java/com/rickjinny/mark/controller/p29_dataandcode/t01_SQLInject/UserDataMapper.java
+++ src/main/java/com/rickjinny/mark/controller/p29_dataandcode/t01_SQLInject/UserDataMapper.java
@@ -2,9 +2,22 @@ package com.rickjinny.mark.controller.p29_dataandcode.t01_SQLInject;
 
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
-public class UserDataMapper {
+public interface UserDataMapper {
 
-    
+    @Select("select id, name from `userdata` where name like '%${name}%'")
+    List<UserData> findByNameWrong(@Param("name") String name);
+
+    List<UserData> findByNamesWrong(@Param("names") String names);
+
+    @Select("select id, name from `userdata` where name like concat('%', #{name}, '%')")
+    List<UserData> findByNameRight(@Param("name") String name);
+
+    List<UserData> findByNamesRight(@Param("names") List<String> names);
+
 }
