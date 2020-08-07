2d18f9f3803e9f5fc19b94c92be0fda26950644e
RickJinny <rickjinny@126.com>
2020-08-07 22:53:58 +0800

    userData
    userData

--- src/main/java/com/rickjinny/mark/controller/p30_sensitivedata/t01_StorePassword/StorePasswordController.java
+++ src/main/java/com/rickjinny/mark/controller/p30_sensitivedata/t01_StorePassword/StorePasswordController.java
@@ -1,9 +1,11 @@
 package com.rickjinny.mark.controller.p30_sensitivedata.t01_StorePassword;
 
+import jodd.crypt.BCrypt;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -84,5 +86,33 @@ public class StorePasswordController {
         userRepository.save(userData);
         log.info("match ? {}", passwordEncoder.matches(password, userData.getPassword()));
         return userData;
+    }
+
+    @RequestMapping(value = "/performance")
+    public void performance() {
+        StopWatch stopWatch = new StopWatch();
+        String password = "abcd1234";
+        stopWatch.start("MD5");
+        DigestUtils.md5Hex(password);
+        stopWatch.stop();
+
+        stopWatch.start("BCrypt(10)");
+        String hash = BCrypt.gensalt(10);
+        BCrypt.hashpw(password, hash);
+        System.out.println(hash);
+        stopWatch.stop();
+
+        stopWatch.start("BCrypt(12)");
+        String hash2 = BCrypt.gensalt(12);
+        BCrypt.hashpw(password, hash2);
+        System.out.println(hash2);
+
+        stopWatch.start("BCrypt(14)");
+        String hash3 = BCrypt.gensalt(14);
+        BCrypt.hashpw(password, hash3);
+        System.out.println(hash3);
+        stopWatch.stop();
+
+        log.info("{}", stopWatch.prettyPrint());
     }
 }
