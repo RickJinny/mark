82f811ca18a51e5d72d97c612df6353502964f9d
RickJinny <rickjinny@126.com>
2020-08-09 08:28:18 +0800

    wrong
    wrong

--- src/main/java/com/rickjinny/mark/controller/p09_numeralcalculations/t01_DangerousDouble/DangerousDoubleController.java
+++ src/main/java/com/rickjinny/mark/controller/p09_numeralcalculations/t01_DangerousDouble/DangerousDoubleController.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
+
 @RestController
 @Slf4j
 @RequestMapping(value = "/dangerousDouble")
@@ -17,6 +19,9 @@ public class DangerousDoubleController {
      * 401.49999999999994
      * 1.2329999999999999
      * no
+     *
+     * 出现这样情况的原因是：计算机是以二进制存储数值的，浮点数也不例外，Java 采用了 IEEE 754 标准实现浮点数的表达和运算。
+     * 对于计算机而言，0.1 无法精确表达，这是浮点数计算造成精度损失的根源。
      */
     private static void wrong1() {
         System.out.println(0.1 + 0.2);
@@ -33,7 +38,26 @@ public class DangerousDoubleController {
         }
     }
 
+    /**
+     * 在使用金额计算的时候，使用 BigDecimal 类型，浮点数精确表达和运算场景，一定使用这个类型。
+     * 不过，在使用 BigDecimal 时有几个坑需要避开。
+     *
+     * 0.3000000000000000166533453693773481063544750213623046875
+     * 0.1999999999999999555910790149937383830547332763671875
+     * 298.0999999999999872102307563181966543197631835937500
+     * 0.3189999999999999857891452847979962825775146484375
+     */
+    private static void wrong2() {
+        System.out.println(new BigDecimal(0.1).add(new BigDecimal(0.2)));
+        System.out.println(new BigDecimal(1.0).subtract(new BigDecimal(0.8)));
+        System.out.println(new BigDecimal(2.981).multiply(new BigDecimal(100)));
+        System.out.println(new BigDecimal(31.9).divide(new BigDecimal(100)));
+    }
+
+    
+
     public static void main(String[] args) {
-        wrong1();
+//        wrong1();
+        wrong2();
     }
 }
