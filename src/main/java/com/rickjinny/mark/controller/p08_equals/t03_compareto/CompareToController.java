ef4570947b068405982c3f59ac8da75fc181d086
RickJinny <rickjinny@126.com>
2020-08-12 08:20:02 +0800

    update
    update

--- src/main/java/com/rickjinny/mark/controller/p08_equals/t03_compareto/CompareToController.java
+++ src/main/java/com/rickjinny/mark/controller/p08_equals/t03_compareto/CompareToController.java
@@ -1,6 +1,7 @@
 package com.rickjinny.mark.controller.p08_equals.t03_compareto;
 
 import com.rickjinny.mark.controller.p08_equals.t03_compareto.bean.Student;
+import com.rickjinny.mark.controller.p08_equals.t03_compareto.bean.StudentRight;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,11 +15,15 @@ import java.util.List;
 @Slf4j
 public class CompareToController {
 
+    /**
+     * binarySearch 方法内部调用了元素的 compareTo 方法进行比较
+     * indexOf 的结果没有问题，
+     */
     public static void wrong() {
         List<Student> list = new ArrayList<>();
-        list.add(new Student(1, "aa"));
-        list.add(new Student(2, "bb"));
-        Student student = new Student(2, "cc");
+        list.add(new Student(1, "zhang"));
+        list.add(new Student(2, "wang"));
+        Student student = new Student(2, "li");
 
         log.info("ArrayList.indexOf");
         int index01 = list.indexOf(student);
@@ -30,7 +35,23 @@ public class CompareToController {
         log.info("index02 = " + index02); // index02 = 0
     }
 
+    public static void right() {
+        List<StudentRight> list = new ArrayList<>();
+        list.add(new StudentRight(1, "zhang"));
+        list.add(new StudentRight(2, "wang"));
+        StudentRight student = new StudentRight(2, "li");
+
+        log.info("ArrayList.indexOf");
+        int index01 = list.indexOf(student);
+        Collections.sort(list);
+        log.info("Collections.binarySearch");
+        int index02 = Collections.binarySearch(list, student);
+        log.info("index01 = " + index01); // index01 = -1
+        log.info("index02 = " + index02); // index02 = -1
+    }
+
     public static void main(String[] args) {
-        wrong();
+//        wrong();
+        right();
     }
 }
--- src/main/java/com/rickjinny/mark/controller/p08_equals/t03_compareto/bean/StudentRight.java
+++ src/main/java/com/rickjinny/mark/controller/p08_equals/t03_compareto/bean/StudentRight.java
@@ -16,6 +16,10 @@ public class StudentRight implements Comparable<StudentRight> {
 
     private String name;
 
+    /**
+     * 确保 compareTo 的比较逻辑和 equals 的实现一致即可。
+     * 通过 Comparator.comparing 方法来实现两个字段的比较。
+     */
     @Override
     public int compareTo(StudentRight other) {
         return Comparator.comparing(StudentRight::getName)
