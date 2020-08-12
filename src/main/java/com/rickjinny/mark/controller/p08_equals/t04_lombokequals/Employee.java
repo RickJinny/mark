79359b8741c97fe84fd2cb672265fe0a1224c274
RickJinny <rickjinny@126.com>
2020-08-12 08:31:27 +0800

    update
    update

---
+++ src/main/java/com/rickjinny/mark/controller/p08_equals/t04_lombokequals/Employee.java
@@ -0,0 +1,19 @@
+package com.rickjinny.mark.controller.p08_equals.t04_lombokequals;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * Lombok 的 @Data 注解会帮我们实现 equals 和 hashcode 方法，但是有继承关系时 Lombok 自动生成的方法可能就不是我们期望的了。
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class Employee extends Person {
+
+    private String company;
+
+    public Employee(String name, String identity, String company) {
+        super(name, identity);
+        this.company = company;
+    }
+}
