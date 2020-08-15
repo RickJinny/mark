ef54810cd399bca18d43f7f4e76aabb698a24944
RickJinny <rickjinny@126.com>
2020-08-15 08:38:17 +0800

    null
    null

--- src/main/java/com/rickjinny/mark/controller/p11_null/t01_AvoidNullPointerException/AvoidNullPointerExceptionController.java
+++ src/main/java/com/rickjinny/mark/controller/p11_null/t01_AvoidNullPointerException/AvoidNullPointerExceptionController.java
@@ -14,6 +14,33 @@ import java.util.concurrent.ConcurrentHashMap;
 @Slf4j
 public class AvoidNullPointerExceptionController {
 
+    private static List<String> wrongMethod(FooService fooService, Integer i, String s, String t) {
+        log.info("result {} {} {} {}", i + 1, s.equals("OK"), s.equals(t),
+                new ConcurrentHashMap<String, String>().put(null, null));
+        if (fooService.getBarService().bar().equals("OK")) {
+            log.info("OK");
+        }
+        return null;
+    }
+
+    /**
+     * 使用 Java 的 Optional 类进行判空，可以消除 if - else 逻辑，使用一行代码进行判空和处理。
+     */
+    private static List<String> rightMethod(FooService fooService, Integer i, String s, String t) {
+        // result 1 false false null
+        log.info("result {} {} {} {}",
+                Optional.ofNullable(i).orElse(0) + 1,
+                "OK".equals(s),
+                Objects.equals(s, t),
+                new HashMap<String, String>().put(null, null));
+
+        Optional.ofNullable(fooService)
+                .map(FooService::getBarService)
+                .filter(barService -> "OK".equals(barService.bar()))
+                .ifPresent(result -> log.info("OK"));
+        return new ArrayList<>();
+    }
+
     @RequestMapping("/wrong")
     public int wrong(@RequestParam(value = "/test", defaultValue = "1111") String test) {
         return wrongMethod(test.charAt(0) == '1' ? null : new FooService(),
@@ -31,26 +58,9 @@ public class AvoidNullPointerExceptionController {
                 .orElse(Collections.emptyList()).size();
     }
 
-    private List<String> wrongMethod(FooService fooService, Integer i, String s, String t) {
-        log.info("result {} {} {} {}", i + 1, s.equals("OK"), s.equals(t),
-                new ConcurrentHashMap<String, String>().put(null, null));
-        if (fooService.getBarService().bar().equals("OK")){
-            log.info("OK");
-        }
-        return null;
-    }
-
-    private List<String> rightMethod(FooService fooService, Integer i, String s, String t) {
-        log.info("result {} {} {} {}",
-                Optional.ofNullable(i).orElse(0) + 1,
-                "OK".equals(s),
-                Objects.equals(s, t),
-                new HashMap<String, String>().put(null, null));
-
-        Optional.ofNullable(fooService)
-                .map(FooService::getBarService)
-                .filter(barService -> "OK".equals(barService.bar()))
-                .ifPresent(result -> log.info("OK"));
-        return new ArrayList<>();
+    public static void main(String[] args) {
+        FooService fooService = new FooService();
+        List<String> list = rightMethod(fooService, null, "a", "b");
+        System.out.println(list);
     }
 }
