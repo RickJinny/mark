32183844f69d6335afa53f73240ea9fad63fd043
RickJinny <rickjinny@126.com>
2020-08-22 23:11:27 +0800

    retry
    retry

---
+++ src/main/java/com/rickjinny/mark/controller/p05_httpinvoke/t03_RibbonRetry/RibbonRetryIssueServerController.java
@@ -0,0 +1,32 @@
+package com.rickjinny.mark.controller.p05_httpinvoke.t03_RibbonRetry;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/ribbonRetryIssueServer")
+public class RibbonRetryIssueServerController {
+
+    @GetMapping(value = "/sms")
+    public void sendSmsWrong(@RequestParam("mobile") String mobile,
+                             @RequestParam("message") String message,
+                             HttpServletRequest request) throws InterruptedException {
+        log.info("{} is called. {} = {}", request.getRequestURL().toString(), mobile, message);
+        TimeUnit.SECONDS.sleep(2);
+    }
+
+    @RequestMapping(value = "/sms")
+    public void sendSmsRight(@RequestParam("mobile") String mobile,
+                             @RequestParam("message") String message,
+                             HttpServletRequest request) throws InterruptedException {
+        log.info("{} is called, {} => {}", request.getRequestURL().toString(), mobile, message);
+        TimeUnit.SECONDS.sleep(2);
+    }
+}
