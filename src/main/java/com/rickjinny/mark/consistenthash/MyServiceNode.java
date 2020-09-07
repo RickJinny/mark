7c218bd1198753e70c621c6292969c0330ca6a78
RickJinny <rickjinny@126.com>
2020-09-07 23:15:23 +0800

    update
    update

--- src/main/java/com/rickjinny/mark/consistenthash/MyServiceNode.java
+++ src/main/java/com/rickjinny/mark/consistenthash/MyServiceNode.java
@@ -1,5 +1,7 @@
 package com.rickjinny.mark.consistenthash;
 
+import java.util.Arrays;
+
 public class MyServiceNode implements Node {
     private final String idc;
     private final String ip;
@@ -26,6 +28,9 @@ public class MyServiceNode implements Node {
         MyServiceNode node02 = new MyServiceNode("idc1", "127.0.0.1", 8081);
         MyServiceNode node03 = new MyServiceNode("idc1", "127.0.0.1", 8082);
         MyServiceNode node04 = new MyServiceNode("idc1", "127.0.0.1", 8083);
-        
+
+        ConsistentHashRouter<MyServiceNode> consistentHashRouter = new ConsistentHashRouter<>(
+                Arrays.asList(node01, node02, node03, node04), 10);
+
     }
 }
