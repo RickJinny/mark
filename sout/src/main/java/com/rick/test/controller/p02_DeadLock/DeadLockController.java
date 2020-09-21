c9d2f40d733ea6649b1697113843f1c34d7212c6
RickJinny <rickjinny@126.com>
2020-09-21 23:22:25 +0800

    Lock
    Lock

--- sout/src/main/java/com/rick/test/controller/p02_DeadLock/DeadLockController.java
+++ sout/src/main/java/com/rick/test/controller/p02_DeadLock/DeadLockController.java
@@ -4,10 +4,44 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.stream.IntStream;
+
 @RestController
 @Slf4j
 @RequestMapping(value = "deadLock")
 public class DeadLockController {
 
-    
+    private ConcurrentHashMap<String, Item> items = new ConcurrentHashMap<>();
+
+    public DeadLockController() {
+        IntStream.rangeClosed(0, 10).forEach(i -> items.put("item" + i, new Item("item" + i)));
+    }
+
+    private boolean createOrder(List<Item> order) {
+        List<ReentrantLock> locks = new ArrayList<>();
+        for (Item item : order) {
+            try {
+                if (item.lock.tryLock(10, TimeUnit.SECONDS)) {
+                    locks.add(item.lock);
+                } else {
+                    locks.forEach(ReentrantLock::unlock);
+                    return false;
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            order.forEach(item -> item.remaining--);
+        } finally {
+            locks.forEach(ReentrantLock::unlock);
+        }
+        return true;
+    }
 }
---
+++ sout/src/main/java/com/rick/test/controller/p02_DeadLock/Item.java
@@ -0,0 +1,14 @@
+package com.rick.test.controller.p02_DeadLock;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+@Data
+@RequiredArgsConstructor
+public class Item {
+    final String name;
+    int remaining = 1000;
+    ReentrantLock lock = new ReentrantLock();
+}
