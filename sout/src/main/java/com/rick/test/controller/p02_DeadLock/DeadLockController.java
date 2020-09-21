d088b9c3be68ada5742b3878a60349ab307e1658
RickJinny <rickjinny@126.com>
2020-09-21 23:37:08 +0800

    Lock
    Lock

--- sout/src/main/java/com/rick/test/controller/p02_DeadLock/DeadLockController.java
+++ sout/src/main/java/com/rick/test/controller/p02_DeadLock/DeadLockController.java
@@ -5,10 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @RestController
@@ -43,5 +46,51 @@ public class DeadLockController {
             locks.forEach(ReentrantLock::unlock);
         }
         return true;
+    }
+
+    private List<Item> createCart() {
+        return IntStream.rangeClosed(1, 3)
+                .mapToObj(i -> "item" + ThreadLocalRandom.current().nextInt(items.size()))
+                .map(name -> items.get(name))
+                .collect(Collectors.toList());
+    }
+
+    @RequestMapping(value = "/wrong")
+    public long wrong() {
+        long begin = System.currentTimeMillis();
+        long success = IntStream.rangeClosed(1, 100).parallel()
+                .mapToObj(i -> {
+                    List<Item> cart = createCart();
+                    return createOrder(cart);
+                })
+                .filter(result -> result)
+                .count();
+        log.info("success:{}, totalRemaining:{}, took:{}ms, items:{}.",
+                success,
+                items.entrySet().stream().map(item -> item.getValue().remaining).reduce(0, Integer::sum),
+                System.currentTimeMillis() - begin,
+                items);
+        return success;
+    }
+
+    @RequestMapping(value = "/right")
+    public long right() {
+        long begin = System.currentTimeMillis();
+        long success = IntStream.rangeClosed(1, 100).parallel()
+                .mapToObj(i -> {
+                    List<Item> cart = createCart().stream()
+                            .sorted(Comparator.comparing(Item::getName))
+                            .collect(Collectors.toList());
+                    return createOrder(cart);
+                })
+                .filter(result -> result)
+                .count();
+
+        log.info("success:{}, totalRemaining:{}, took:{}ms, items:{}",
+                success,
+                items.entrySet().stream().map(item -> item.getValue().remaining).reduce(0, Integer::sum),
+                System.currentTimeMillis() - begin,
+                items);
+        return success;
     }
 }
