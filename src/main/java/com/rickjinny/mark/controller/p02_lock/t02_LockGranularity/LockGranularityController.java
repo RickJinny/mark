48b09c804e0e87d1f1433bcd275039fba2260e9c
RickJinny <rickjinny@126.com>
2020-10-14 00:13:20 +0800

    update
    update

--- src/main/java/com/rickjinny/mark/controller/p02_lock/t02_LockGranularity/LockGranularityController.java
+++ src/main/java/com/rickjinny/mark/controller/p02_lock/t02_LockGranularity/LockGranularityController.java
@@ -16,6 +16,9 @@ public class LockGranularityController {
 
     private List<Integer> data = new ArrayList<>();
 
+    /**
+     * 不涉及共享资源的慢方法
+     */
     private void slow() {
         try {
             TimeUnit.MILLISECONDS.sleep(10);
@@ -24,10 +27,14 @@ public class LockGranularityController {
         }
     }
 
+    /**
+     * 错误的加锁方法
+     */
     @RequestMapping(value = "/wrong")
     public int wrong() {
         long begin = System.currentTimeMillis();
         IntStream.rangeClosed(1, 1000).parallel().forEach(i -> {
+            // 加锁粒度太粗了
             synchronized (this) {
                 slow();
                 data.add(i);
@@ -37,11 +44,15 @@ public class LockGranularityController {
         return data.size();
     }
 
+    /**
+     * 正确的加锁方法
+     */
     @RequestMapping(value = "/right")
     public int right() {
         long begin = System.currentTimeMillis();
         IntStream.rangeClosed(1, 1000).parallel().forEach(i -> {
             slow();
+            // 只对 List 加锁
             synchronized (data) {
                 data.add(i);
             }
