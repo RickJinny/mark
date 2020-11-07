6c856345b0637b5748a7e33f89a7bd830eb0f1b9
RickJinny <rickjinny@126.com>
2020-11-07 16:51:07 +0800

    ddd
    ddd

---
+++ Simple-DDD/src/test/java/com/mark/SimpleDddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleDddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
