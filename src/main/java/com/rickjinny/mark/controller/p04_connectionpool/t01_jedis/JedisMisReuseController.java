329a02f64182a8a0ce4b687fba05ff0fdd304f7b
RickJinny <rickjinny@126.com>
2020-11-22 18:20:48 +0800

    connection pool
    connection pool

---
+++ src/main/java/com/rickjinny/mark/controller/p04_connectionpool/t01_jedis/JedisMisReuseController.java
@@ -0,0 +1,88 @@
+package com.rickjinny.mark.controller.p04_connectionpool.t01_jedis;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+import javax.annotation.PostConstruct;
+import java.util.concurrent.TimeUnit;
+
+@RestController
+@RequestMapping(value = "/jedismisreuse")
+@Slf4j
+public class JedisMisReuseController {
+
+    private static JedisPool jedisPool = new JedisPool("127.0.0.1", 6379);
+
+    @PostConstruct
+    public void init() {
+        try (Jedis jedis = new Jedis("127.0.0.1", 6379)) {
+            Assert.isTrue("OK".equals(jedis.set("a", "1")), "set a = 1 return OK");
+            Assert.isTrue("OK".equals(jedis.set("b", "2")), "set b = 2 return OK");
+        }
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            jedisPool.close();
+        }));
+    }
+
+    @RequestMapping(value = "/wrong", method = RequestMethod.GET)
+    public void wrong() {
+        Jedis jedis = new Jedis("127.0.0.1", 6379);
+        new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                String result = jedis.get("a");
+                if (!"1".equals(result)) {
+                    log.warn("Expect a to be 1 but found {}", result);
+                    return;
+                }
+            }
+        }).start();
+
+
+        new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                String result = jedis.get("b");
+                if (!"2".equals(result)) {
+                    log.warn("Expect b to be 2 but found {}", result);
+                    return;
+                }
+            }
+
+        }).start();
+    }
+
+    @GetMapping(value = "/right")
+    public void right() throws InterruptedException {
+        new Thread(() -> {
+            try (Jedis jedis = jedisPool.getResource()) {
+                for (int i = 0; i < 1000; i++) {
+                    String result = jedis.get("a");
+                    if (!"a".equals(result)) {
+                        log.warn("Expect a to be 1 but found {}", result);
+                        return;
+                    }
+                }
+            }
+        }).start();
+
+        new Thread(() -> {
+            try (Jedis jedis = jedisPool.getResource()) {
+                for (int i = 0; i < 1000; i++) {
+                    String result = jedis.get("b");
+                    if (!"2".equals(result)) {
+                        log.warn("Expect b to be 2 but found {}", result);
+                        return;
+                    }
+                }
+            }
+        }).start();
+
+        TimeUnit.SECONDS.sleep(5);
+    }
+}
