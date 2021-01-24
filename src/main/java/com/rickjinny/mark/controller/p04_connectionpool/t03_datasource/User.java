9ddd168e87c4b1be8b2aba12a6c09cb4ec98fa40
RickJinny <rickjinny@126.com>
2021-01-24 23:04:39 +0800

    update
    update

---
+++ src/main/java/com/rickjinny/mark/controller/p04_connectionpool/t03_datasource/User.java
@@ -0,0 +1,20 @@
+package com.rickjinny.mark.controller.p04_connectionpool.t03_datasource;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    
+}
