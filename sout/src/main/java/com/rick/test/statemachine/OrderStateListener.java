31fb8754539b10273c4e631cdb34e8cbff4d1c3c
RickJinny <rickjinny@126.com>
2021-02-14 16:09:57 +0800

    Order State Machine
    Order State Machine

---
+++ sout/src/main/java/com/rick/test/statemachine/OrderStateListener.java
@@ -0,0 +1,31 @@
+package com.rick.test.statemachine;
+
+import org.springframework.messaging.Message;
+import org.springframework.statemachine.annotation.OnTransition;
+import org.springframework.statemachine.annotation.WithStateMachine;
+import org.springframework.stereotype.Component;
+
+@Component("orderStateListener")
+@WithStateMachine(name = "orderStateMachine")
+public class OrderStateListener {
+
+    @OnTransition(source = "WAIT_PAYMENT", target = "WAIT_DELIVER")
+    public boolean payTransition(Message<OrderStatusChangeEvent> message) {
+        Order order = (Order) message.getHeaders().get("order");
+        if (order != null) {
+            order.setStatus(OrderStatus.WAIT_DELIVER);
+        }
+        System.out.println("支付 headers = " + message.getHeaders().toString());
+        return true;
+    }
+
+    @OnTransition(source = "WAIT_RECEIVE", target = "FINISH")
+    public boolean deliverTransition(Message<OrderStatusChangeEvent> message) {
+        Order order = (Order) message.getHeaders().get("order");
+        if (order != null) {
+            order.setStatus(OrderStatus.FINISH);
+        }
+        System.out.println("收货 headers = " + message.getHeaders().toString());
+        return true;
+    }
+}
