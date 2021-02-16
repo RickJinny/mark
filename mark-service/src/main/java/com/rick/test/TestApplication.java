b2eecf856656825fe4c020ec061e0bb152aa424f
RickJinny <rickjinny@126.com>
2021-02-16 23:04:30 +0800

    datasource
    datasource

--- mark-service/src/main/java/com/rick/test/TestApplication.java
+++ mark-service/src/main/java/com/rick/test/TestApplication.java
@@ -3,9 +3,10 @@ package com.rick.test;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
-@MapperScan("com.rick.test.dao.mapper")
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@MapperScan(basePackages = "com.rick.test.dao.mapper")
 public class TestApplication {
 
     public static void main(String[] args) {
