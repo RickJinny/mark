f2a0609e9f95326c6eded8b02e6baf21575d09a5
RickJinny <rickjinny@126.com>
2021-03-07 01:09:35 +0800

    update
    update

---
+++ mark-service/src/main/java/com/rick/test/dao/model/Order.java
@@ -0,0 +1,55 @@
+package com.rick.test.dao.model;
+
+import java.math.BigDecimal;
+
+public class Order {
+    private Long id;
+
+    private Long orderId;
+
+    private BigDecimal price;
+
+    private Long userId;
+
+    private Integer status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+}
