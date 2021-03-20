a56056c55a5f230c0829ce1f5de439651451874b
RickJinny <rickjinny@126.com>
2021-03-20 14:49:27 +0800

    分库分表

--- mark-service/src/main/java/com/rick/test/dao/model/Order.java
+++ mark-service/src/main/java/com/rick/test/dao/model/Order.java
@@ -1,6 +1,7 @@
 package com.rick.test.dao.model;
 
 import java.math.BigDecimal;
+import java.util.Date;
 
 public class Order {
     private Long id;
@@ -14,6 +15,10 @@ public class Order {
     private BigDecimal price;
 
     private Integer status;
+
+    private Date createTime;
+
+    private Date updateTime;
 
     public Long getId() {
         return id;
@@ -61,5 +66,21 @@ public class Order {
 
     public void setStatus(Integer status) {
         this.status = status;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
     }
 }
