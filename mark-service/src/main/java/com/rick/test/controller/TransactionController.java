9b6ba6739644fe79d9a9d24b41c7b8706a312e81
RickJinny <rickjinny@126.com>
2021-05-30 17:47:17 +0800

    update
    update

--- mark-service/src/main/java/com/rick/test/controller/TransactionController.java
+++ mark-service/src/main/java/com/rick/test/controller/TransactionController.java
@@ -3,6 +3,7 @@ package com.rick.test.controller;
 import com.rick.service.TransactionService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,7 +15,7 @@ public class TransactionController {
     @Autowired
     private TransactionService transactionService;
 
-    @RequestMapping(value = "/transfer")
+    @PostMapping(value = "/transfer")
     public String transfer(String fromName, String toName, Integer money) {
         try {
             transactionService.transfer(fromName, toName, money);
