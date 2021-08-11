c91b38b88c2f5a93b2ad9611c384c168c336c17a
RickJinny <rickjinny@126.com>
2021-08-11 23:52:24 +0800

    Lettuce
    Lettuce

---
+++ mark-service/src/main/java/com/rick/test/controller/Redis_Lettuce_Controller.java
@@ -0,0 +1,14 @@
+package com.rick.test.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/lettuce")
+public class Redis_Lettuce_Controller {
+
+
+
+}
