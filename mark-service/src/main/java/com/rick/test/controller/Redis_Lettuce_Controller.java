84bbdd7990144d582a0a6308560093df11bfd6a9
RickJinny <rickjinny@126.com>
2021-08-14 16:23:26 +0800

    Letture
    Letture

--- mark-service/src/main/java/com/rick/test/controller/Redis_Lettuce_Controller.java
+++ mark-service/src/main/java/com/rick/test/controller/Redis_Lettuce_Controller.java
@@ -1,7 +1,14 @@
 package com.rick.test.controller;
 
+import com.rick.common.ServerResponse;
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisCommands;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -9,6 +16,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/lettuce")
 public class Redis_Lettuce_Controller {
 
+    @ResponseBody
+    @PostMapping(value = "/test")
+    public ServerResponse<String> testRedisLettuce() {
+        RedisClient redisClient = RedisClient.create(RedisURI.Builder.redis("192.168.0.121", 6379).build());
+        StatefulRedisConnection<String, String> connection = redisClient.connect();
+        RedisCommands<String, String> syncCommands = connection.sync();
+        syncCommands.set("lettuce_key01", "xiaowang");
+        String value01 = syncCommands.get("lettuce_key01");
+        log.info("lettuce_key01: {}", value01);
 
+        connection.close();
+        redisClient.shutdown();
+        return ServerResponse.createBySuccess("success");
+    }
 
 }
