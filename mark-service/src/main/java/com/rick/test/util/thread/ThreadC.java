529ddd0f53127f54cd8b46cd27bbc874dd9dbb7e
RickJinny <rickjinny@126.com>
2021-10-09 16:46:10 +0800

    Thread
    Thread

---
+++ mark-service/src/main/java/com/rick/test/util/thread/ThreadC.java
@@ -0,0 +1,14 @@
+package com.rick.test.util.thread;
+
+import java.util.concurrent.Callable;
+
+public class ThreadC implements Callable<String> {
+
+    @Override
+    public String call() throws Exception {
+        Thread.sleep(500);
+        System.out.println("这是线程C");
+        return "线程C";
+    }
+
+}
