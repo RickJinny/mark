1f4b407453fe28ab70f6d3634980e130fe1b39e9
RickJinny <rickjinny@126.com>
2021-10-10 10:35:47 +0800

    Final
    Final

---
+++ mark-service/src/main/java/com/rick/test/util/final0/FinalTest.java
@@ -0,0 +1,26 @@
+package com.rick.test.util.final0;
+
+public class FinalTest {
+
+    public static final int aFinal = 2;
+
+    public static int b = 3;
+
+    public static void main(String[] args) {
+
+        b = aFinal + b;
+        System.out.println(b);
+        System.out.println(aFinal);
+
+        Student student = new Student();
+        student.setName("小明");
+        student.setAge(20);
+        String student1 = getStudent(student);
+        System.out.println(student1);
+    }
+
+    public static String getStudent(final Student student) {
+        return student.getName() + " : " + student.getAge();
+    }
+
+}
---
+++ mark-service/src/main/java/com/rick/test/util/final0/Student.java
@@ -0,0 +1,12 @@
+package com.rick.test.util.final0;
+
+import lombok.Data;
+
+@Data
+public class Student {
+
+    private String name;
+
+    private Integer age;
+
+}
