2f6f3ef5da2a1e455091de5f6602a3fdd6c41246
RickJinny <rickjinny@126.com>
2021-10-20 23:35:16 +0800

    Semaphore
    Semaphore

---
+++ mark-service/src/main/java/com/rick/test/util/tool/SemaphoreDemo.java
@@ -0,0 +1,42 @@
+package com.rick.test.util.tool;
+
+import java.util.concurrent.Semaphore;
+
+public class SemaphoreDemo {
+
+    public static void main(String[] args) {
+        int n = 8;
+        // 5 是机器数目
+        Semaphore semaphore = new Semaphore(5);
+        for (int i = 0; i < n; i++) {
+            new Worker(i, semaphore).start();
+        }
+    }
+
+    private static class Worker extends Thread {
+
+        private int num;
+        private Semaphore semaphore;
+
+        public Worker(int num, Semaphore semaphore) {
+            this.num = num;
+            this.semaphore = semaphore;
+        }
+
+        @Override
+        public void run() {
+            try {
+                // 在子线程里控制资源占用
+                semaphore.acquire();
+                System.out.println("工人" + this.num + "占用一个机器在生产...");
+                Thread.sleep(2000);
+                System.out.println("工人" + this.num + "释放出机器");
+                // 在子线程里控制释放资源占用
+                semaphore.release();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
