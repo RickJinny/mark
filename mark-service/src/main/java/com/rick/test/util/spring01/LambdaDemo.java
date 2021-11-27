a488bc959c8df6f75bc29771881c5225a2060859
RickJinny <rickjinny@126.com>
2021-11-27 17:56:23 +0800

    1
    1

--- mark-service/src/main/java/com/rick/test/util/spring01/LambdaDemo.java
+++ mark-service/src/main/java/com/rick/test/util/spring01/LambdaDemo.java
@@ -2,8 +2,10 @@ package com.rick.test.util.spring01;
 
 public class LambdaDemo {
 
+    /**
+     * 函数签名: 返回类型 + 函数名 + 参数类型的列表
+     */
     interface MathOperation {
-        // 函数签名: 返回类型 + 函数名 + 参数类型的列表
         int operation(int a, int b);
     }
 
