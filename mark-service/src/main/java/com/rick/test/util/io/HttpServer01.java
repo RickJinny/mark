26c2657e50a9999b9998838ef802bdb27467a058
RickJinny <rickjinny@126.com>
2022-01-28 21:39:07 +0800

    HttpServer
    HttpServer

---
+++ mark-service/src/main/java/com/rick/test/util/io/HttpServer01.java
@@ -0,0 +1,37 @@
+package com.rick.test.util.io;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class HttpServer01 {
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(8801);
+        while (true) {
+            try {
+                Socket socket = serverSocket.accept();
+                service(socket);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static void service(Socket socket) {
+        try {
+            Thread.sleep(20);
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
+            printWriter.println("HTTP/1.1 200 OK");
+            printWriter.println("Content-Type:text/html;charset=utf-8");
+            printWriter.println();
+            printWriter.write("hello, nio");
+            printWriter.close();
+            socket.close();
+        } catch (Exception e) {
+
+        }
+    }
+
+}
